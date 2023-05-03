519c5eb0afe61e74f5d44261bf6eac285786ba8d
Caio Chaves <caiovitorlchaves@gmail.com>
2023-05-03 14:31:19 -0300

    Novos Arquivos

---
+++ Testes.py
@@ -0,0 +1,26 @@
+# nota1 = float(input("Digite sua primeira nota: "))
+# nota2 = float(input("Digite sua segunda nota: "))
+# nota3 = float(input("Digite sua terceira nota: "))
+
+# media = ((nota1 + nota2 + nota3) / 3)
+
+# if (media >= 7 ):
+#     print("Aprovado")
+
+# else:
+#     print("Reprovado")
+
+# a = 7==7
+# print(type (a))
+
+# nome = "caio"
+# sobrenome = "vitor"
+# sobrenome1 = "lopes"
+# sobrenome2 = "chaves"
+# print (nome, sobrenome, sobrenome1, sobrenome2)
+
+valor1 = float(input("Digite seu primeiro valor: "))
+valor2 = float(input("Digite seu segundo valor: "))
+total = (valor1 + valor2)
+
+print ('Seu valor total é:'  + total +  'Reais')
