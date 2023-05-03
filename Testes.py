25572c0595fbceb14c731793f87f9e51e787567a
Caio Chaves <caiovitorlchaves@gmail.com>
2023-05-03 15:52:27 -0300

    Novos Arquivos

--- Testes.py
+++ Testes.py
@@ -19,8 +19,25 @@
 # sobrenome2 = "chaves"
 # print (nome, sobrenome, sobrenome1, sobrenome2)
 
-valor1 = float(input("Digite seu primeiro valor: "))
-valor2 = float(input("Digite seu segundo valor: "))
-total = (valor1 + valor2)
+# valor1 = float(input("Digite seu primeiro valor: "))
+# valor2 = float(input("Digite seu segundo valor: "))
+# total = (valor1 + valor2)
 
-print ('Seu valor total é:'  + total +  'Reais')
+# print ('Seu valor total é: ',total, 'reais')
+
+# a = 5
+# b = 5
+# print(a + b, a - b, a * b, a / b)
+
+# a = 3+7
+# resultado = a==10
+# print(a)
+# print(resultado)
+
+# a = 5 + 5 \
+# + 5
+
+# print(a)
+
+# a = 'Caio'
+# print(a[0])
